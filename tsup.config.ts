1e3f025b1adff420705c86bba99668bc7604dd64
AbabilCore <ababilhossain36@gmail.com>
2025-04-21 13:18:35 +0600

    feat: initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm"],
+    outExtension: () => ({ js: ".mjs" }),
+    dts: true,
+    minify: true,
+    clean: true,
+    esbuildOptions(options) {
+      options.mangleProps = /^_/;
+      options.mangleQuoted = true;
+    },
+  },
+  {
+    entry: ["src/index.cts"],
+    format: ["cjs"],
+    outExtension: () => ({ js: ".cjs" }),
+    dts: false,
+    minify: true,
+    clean: false,
+    esbuildOptions(options) {
+      options.mangleProps = /^_/;
+      options.mangleQuoted = true;
+    },
+  },
+]);

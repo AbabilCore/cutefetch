de1a62257ff798ed856b431f0e80daf8c5d0450c
AbabilCore <ababilspark@gmail.com>
2025-04-22 17:48:51 +0600

    feat(helper): add RequestHandler class to handle http request

---
+++ src/helper/_request.handler.ts
@@ -0,0 +1,138 @@
+import {
+  CF_ERROR,
+  CuteFetchResponse,
+  defaultTimeout,
+  IRequestOptions,
+  NormalizedReqOptions,
+  ReqParams,
+} from "@/shared";
+
+export class RequestHandler {
+  private _timout = (timeout: number) => {
+    const aborter = new AbortController();
+
+    const timerId = setTimeout(() => {
+      aborter.abort(
+        new CF_ERROR(
+          `Timout Failed! '${timeout} ms' consider using larger timeout value`
+        )
+      );
+
+      clearTimeout(timerId);
+    }, timeout);
+
+    return aborter;
+  };
+
+  private _normalized_request_options = (
+    options?: IRequestOptions
+  ): NormalizedReqOptions => {
+    const { body, cache, credentials, headers, method, mode, timeout } =
+      options || {};
+
+    return Object.fromEntries(
+      Object.entries({
+        body,
+        cache,
+        credentials,
+        headers,
+        method,
+        mode,
+        signal: this._timout(timeout || defaultTimeout).signal,
+      }).filter(([, value]) => value !== undefined)
+    );
+  };
+
+  private _parse_response = async (response: Response) => {
+    const contentType = response.headers.get("Content-Type");
+
+    if (!contentType)
+      return `Unable to detect Content-Type. \n Consider using 'cf.extra()' to handle this manually.`;
+
+    if (contentType.startsWith("application/json")) {
+      try {
+        return await response.json();
+      } catch (error: any) {
+        return `Failed to parse JSON! The requested source might be sending invalid JSON data.\n${
+          error?.message || error
+        }`;
+      }
+    }
+
+    if (contentType.startsWith("text/")) {
+      try {
+        return await response.text();
+      } catch (error: any) {
+        return `Failed to parsing TEXT ! Maybe requested sources sending invalid text data\n${
+          error?.message || error
+        }`;
+      }
+    }
+
+    if (
+      contentType.startsWith("image/") ||
+      contentType.startsWith("audio/") ||
+      contentType.startsWith("video/") ||
+      contentType === "application/pdf" ||
+      contentType === "application/octet-stream"
+    ) {
+      return await response.blob();
+    }
+
+    throw new CF_ERROR(
+      `Unknown Content-Type: ${contentType}. Unable to parse response.\nConsider using 'cf.extra()' to handle ${contentType}.`
+    );
+  };
+
+  private _cute_fetch_response = async (
+    res: Response
+  ): Promise<CuteFetchResponse> => {
+    const result = await this._parse_response(res);
+    return {
+      status: res.status,
+      statusText: res.statusText,
+      data: res.ok ? result : null,
+      error: !res.ok ? result : null,
+    };
+  };
+
+  //.................................
+  // Handle All Request
+  //..................................
+  protected reqGET = async ({ url, options }: ReqParams) => {
+    options = this._normalized_request_options(options);
+
+    const resonse = await fetch(url, options);
+    return await this._cute_fetch_response(resonse);
+  };
+
+  protected reqPOST = async ({ url, options }: ReqParams) => {
+    options = this._normalized_request_options(options);
+    const resonse = await fetch(url, options);
+    return await this._cute_fetch_response(resonse);
+  };
+
+  protected reqPUT = async ({ url, options }: ReqParams) => {
+    options = this._normalized_request_options(options);
+    const resonse = await fetch(url, options);
+    return await this._cute_fetch_response(resonse);
+  };
+
+  protected reqPATCH = async ({ url, options }: ReqParams) => {
+    options = this._normalized_request_options(options);
+    const resonse = await fetch(url, options);
+    return await this._cute_fetch_response(resonse);
+  };
+
+  protected reqDELETE = async ({ url, options }: ReqParams) => {
+    options = this._normalized_request_options(options);
+    const resonse = await fetch(url, options);
+    return await this._cute_fetch_response(resonse);
+  };
+
+  protected reqEXTRA = async ({ url, options }: ReqParams) => {
+    options = this._normalized_request_options(options);
+
+    return await fetch(url, options);
+  };
+}

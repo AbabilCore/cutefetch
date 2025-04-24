e65a38ffbf7d7380912ab130c834fdb5e4de1587
AbabilCore <ababilspark@gmail.com>
2025-04-25 04:24:07 +0600

    feat: add types for CuteFetch response handling and inspection
    - Added , , , , and  types.
    - Introduced type-safe configuration for handling response transformations and error transformations.
    - Improved inspection capabilities with customizable rules for logging request details.

--- src/shared/constants/_types.ts
+++ src/shared/constants/_types.ts
@@ -1,3 +1,5 @@
+import { InspectObject } from "@/shared";
+
 export type RequestMethod =
   | "GET"
   | "POST"
@@ -16,3 +18,38 @@ export type ConfigProps =
   | "credentials"
   | "mode"
   | "cache";
+
+export type CuteFetchResponse = {
+  status: number;
+  statusText: string;
+  data?: any;
+  error?: any;
+};
+
+export type TransformResponse = (dataFromServer: any) => any;
+
+export type TransformErrorResponse = (errorFromServer: any) => any;
+
+type InspectRule = {
+  full_url?: boolean;
+  baseURL?: boolean;
+  methods?: boolean;
+  method?: boolean;
+  headers?: boolean;
+  query?: boolean;
+  body?: boolean;
+  timeout?: boolean;
+  mode?: boolean;
+  cors?: boolean;
+  cache?: boolean;
+  credentials?: boolean;
+};
+
+export type Inspect = () => {
+  name_space?: string;
+  rule?: InspectRule;
+  callback: (result: {
+    readonly extract: () => InspectObject;
+    [key: string]: InspectObject | (() => InspectObject);
+  }) => void;
+};

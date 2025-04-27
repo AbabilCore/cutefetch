24d7f1d6042cd249f6b25475888e340ead03d714
AbabilCore <ababilspark@gmail.com>
2025-04-27 22:01:25 +0600

    feat: add generator module for generate README.md by handlebars

---
+++ docs/generator.ts
@@ -0,0 +1,37 @@
+import HB from "handlebars";
+import { basename, join } from "path";
+import { readFile, writeFile } from "fs/promises";
+import { template_index, readme_data, partials, assets } from "@/docs/shared";
+import { ROOT_DIR, tcWrapper } from "@/module";
+
+const register_partials = tcWrapper(async () => {
+  for (const partial of partials) {
+    const name = basename(partial.path).replace(".hbs", "");
+    const content = await readFile(partial.path, "utf-8");
+    HB.registerPartial(name, content);
+  }
+});
+
+const register_helper = tcWrapper(async () => {
+  HB.registerHelper("asset", (key: string) => {
+    return assets[key];
+  });
+});
+
+const generateREADME = tcWrapper(async () => {
+  // register hbs extra features
+  await register_partials();
+  await register_helper();
+
+  const index = await readFile(template_index, "utf-8");
+
+  const template = HB.compile(index);
+
+  const template_content = template({
+    ...(await readme_data()),
+  });
+
+  await writeFile(join(ROOT_DIR, "README.md"), template_content, "utf-8");
+});
+
+export default generateREADME;

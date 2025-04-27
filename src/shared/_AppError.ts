80a21d97e2e2ec4145eb46b1bedc3c2ee929a941
AbabilCore <ababilspark@gmail.com>
2025-04-27 22:13:21 +0600

    refactor: remove AppError and logger from src/shared

--- src/shared/_AppError.ts
+++
@@ -1,9 +0,0 @@
-export class CF_ERROR extends Error {
-  public name: string;
-
-  constructor(message: string) {
-    super(message);
-    this.name = "CuteFetch";
-    this.message = message;
-  }
-}

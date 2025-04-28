66c3d0ced428db72149609899e35ec6702504a47
AbabilCore <ababilspark@gmail.com>
2025-04-28 12:07:08 +0600

    refactor: fetch createdAt and lastModified from global modules

--- docs/shared/_data.ts
+++ docs/shared/_data.ts
@@ -1,11 +1,11 @@
-import { pkg_data } from "@/module";
+import { CREATED_AT, pkg_data, TODAY } from "@/module";
 import { assets } from "@/docs/shared";
 
 export const readme_data = async () => {
   return {
     pkg: await pkg_data(),
     assets,
-    createdAt: "2024-02-29",
-    lastModified: new Date().toISOString().split("T")[0],
+    createdAt: CREATED_AT,
+    lastModified: TODAY,
   };
 };

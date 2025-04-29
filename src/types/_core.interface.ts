a63e203458f67818441d23807979cf0f885ae431
AbabilCore <ababilhossain36@gmail.com>
2025-04-29 23:05:01 +0600

    refactor: create dedicated types folder to organize shared types

---
+++ src/types/_core.interface.ts
@@ -0,0 +1,57 @@
+import {
+  Inspect,
+  RequestMethod,
+  TransformErrorResponse,
+  TransformResponse,
+} from "@/src/types";
+
+export interface IConfig
+  extends Pick<RequestInit, "credentials" | "mode" | "cache"> {
+  baseURL?: string;
+  methods?: RequestMethod[];
+  timeout?: number;
+  headers?: HeadersInit;
+}
+
+export interface IRequestOptions extends IConfig {
+  query?: Record<string, string>;
+  method?: RequestMethod;
+  body?: BodyInit | null;
+  transformResponse?: TransformResponse;
+  transformErrorResponse?: TransformErrorResponse;
+  inspect?: Inspect;
+}
+
+export interface IRequestOptionsExtra
+  extends Omit<
+    IRequestOptions,
+    "transformResponse" | "transformErrorResponse"
+  > {}
+
+export interface InitiatorParams {
+  path: string;
+  method: RequestMethod;
+  options?: IRequestOptions;
+}
+
+export interface GenURLParams {
+  path: string;
+  baseURL?: string;
+  query?: Record<string, string>;
+}
+
+export interface ReqParams {
+  url: string;
+  options?: Omit<IRequestOptions, "query" | "baseURL" | "methods">;
+}
+
+export interface NormalizedReqOptions
+  extends Omit<IRequestOptions, "methods" | "baseURL" | "timeout" | "query"> {}
+
+export interface InspectObject
+  extends Omit<
+    IRequestOptions,
+    "transformResponse" | "transformErrorResponse"
+  > {
+  full_url?: string;
+}
---
+++ src/types/_core.types.ts
@@ -0,0 +1,55 @@
+import { InspectObject } from "@/src/types";
+
+export type RequestMethod =
+  | "GET"
+  | "POST"
+  | "PUT"
+  | "DELETE"
+  | "PATCH"
+  | "HEAD"
+  | "OPTIONS"
+  | "EXTRA";
+
+export type ConfigProps =
+  | "baseURL"
+  | "methods"
+  | "timeout"
+  | "headers"
+  | "credentials"
+  | "mode"
+  | "cache";
+
+export type CuteFetchResponse = {
+  status: number;
+  statusText: string;
+  data?: any;
+  error?: any;
+};
+
+export type TransformResponse = (dataFromServer: any) => any;
+
+export type TransformErrorResponse = (errorFromServer: any) => any;
+
+type InspectRule = {
+  full_url?: boolean;
+  baseURL?: boolean;
+  methods?: boolean;
+  method?: boolean;
+  headers?: boolean;
+  query?: boolean;
+  body?: boolean;
+  timeout?: boolean;
+  mode?: boolean;
+  cors?: boolean;
+  cache?: boolean;
+  credentials?: boolean;
+};
+
+export type Inspect = () => {
+  name_space?: string;
+  rule?: InspectRule;
+  callback: (result: {
+    readonly extract: () => InspectObject;
+    [key: string]: InspectObject | (() => InspectObject);
+  }) => void;
+};

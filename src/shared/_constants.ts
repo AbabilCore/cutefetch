01205667203e092f8ea2fcde7f71ff658b914e5e
AbabilCore <ababilhossain36@gmail.com>
2025-04-29 23:08:53 +0600

    refactor: move _constants.ts to @/src/shared directory

---
+++ src/shared/_constants.ts
@@ -0,0 +1,53 @@
+import { ConfigProps, RequestMethod } from "@/src/types";
+
+export const credentialsProps: RequestCredentials[] = [
+  "include",
+  "same-origin",
+  "omit",
+];
+
+export const modeProps: RequestMode[] = [
+  "cors",
+  "no-cors",
+  "same-origin",
+  "navigate",
+];
+
+export const cacheProps: RequestCache[] = [
+  "default",
+  "no-cache",
+  "reload",
+  "force-cache",
+  "only-if-cached",
+  "no-store",
+];
+
+export const methodsProps: RequestMethod[] = [
+  "GET",
+  "POST",
+  "PUT",
+  "DELETE",
+  "PATCH",
+  "HEAD",
+  "OPTIONS",
+  "EXTRA",
+];
+
+export const bodyLessMethodProps: RequestMethod[] = [
+  "GET",
+  "HEAD",
+  "OPTIONS",
+  "DELETE",
+];
+
+export const confgPropsProps: ConfigProps[] = [
+  "baseURL",
+  "cache",
+  "credentials",
+  "methods",
+  "mode",
+  "timeout",
+  "headers",
+];
+
+export const defaultTimeout: number = 5000;

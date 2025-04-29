7ac6e4ff6fca2913f3aa63be5acc17e7db9e0ab6
AbabilCore <ababilspark@gmail.com>
2025-04-29 22:52:46 +0600

    feat: add dir_tree in readme_data

--- docs/shared/_data.ts
+++ docs/shared/_data.ts
@@ -1,11 +1,12 @@
-import { CREATED_AT, pkg_data, TODAY } from "@/module";
+import { get_dir_tree, CREATED_AT, pkg_data, TODAY } from "@/module";
 import { assets } from "@/docs/shared";
 
 export const readme_data = async () => {
   return {
-    pkg: await pkg_data(),
     assets,
     createdAt: CREATED_AT,
     lastModified: TODAY,
+    pkg: pkg_data(),
+    dir_tree: get_dir_tree(),
   };
 };
